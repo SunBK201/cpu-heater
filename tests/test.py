3122279a58091226ae0fbb2419abf998db7605ff
SunBK201 <sunbk201gm@gmail.com>
2025-02-14 15:09:16 +0800

    chore: modify test case

--- tests/test.py
+++ tests/test.py
@@ -7,9 +7,17 @@ def adder(x, y):
 
 def test_cpu_heater():
     args_list = [(i, i) for i in range(114514)]
-    results = cpu_heater.multiprocess(adder, args_list, max_workers=8, show_progress=True, timeout=5)
+    results = cpu_heater.multiprocess(
+        adder,
+        args_list,
+        max_workers=8,
+        show_progress=True,
+        desc="test",
+        not_none=True,
+        extend_mode=False,
+    )
     assert sorted(results) == sorted([i + i for i in range(114514)])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_cpu_heater()
